432c15b580f310f3d016ec273be533dec7fb71f7
chanToTo <38659554+chanToTo@users.noreply.github.com>
2020-02-23 22:15:31 +0800

    jvm + hashmap部分

---
+++ src/main/java/com/orange/jvm/BinarySearch.java
@@ -0,0 +1,51 @@
+package com.orange.jvm;
+
+/**
+ * @author orangeC
+ * @description
+ * @date 2020/2/23 16:18
+ */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = {12,45,67,68,69,101,120};
+//        System.out.println(get(arr, 0, arr.length));
+        System.out.println(binarySea(arr, 130));
+    }
+
+    public static int binarySea(int[] arr, int num){
+        int low = 0;
+        int height = arr.length - 1;
+
+        while (low <= height) {
+            int middle = low + (height - low) /2;
+
+            if (num == arr[middle]) {
+                return middle;
+            } else if (num > arr[middle]) {
+                low = middle + 1;
+            } else if (num < arr[middle]) {
+                height = middle - 1;
+            }
+        }
+        return -1;
+    }
+
+    /*public static int get(int[] arr, int start, int end) {
+        int x = 70;
+        if (x < arr[0]) {
+            return -1;
+        }
+        int middle = (start + end) / 2;
+        if (middle == start) {
+            return -1;
+        }
+        if (arr[middle] == x) {
+            return middle;
+        } else if (x > arr[middle]) {
+            return get(arr, middle, end);
+        } else {
+            return get(arr, start, middle);
+        }
+    }*/
+}
---
+++ src/main/java/com/orange/jvm/T2.java
@@ -0,0 +1,18 @@
+package com.orange.jvm;
+
+/**
+ * @author orangeC
+ * @description
+ * @date 2020/2/23 9:44
+ */
+public class T2 {
+
+    public static void main(String[] args) {
+        // 最大 -Xmx
+        System.out.println(Runtime.getRuntime().maxMemory());
+        // 默认初始大小 -Xms
+        System.out.println(Runtime.getRuntime().totalMemory());
+        // cpu核数
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
